3d7a8f20ef657dcaf8d173ec9ac9af8a9fe35328
Zoe <183669693+Sikzyo@users.noreply.github.com>
2026-01-28 10:36:40 -0500

    feat: agregar colores a la app

---
+++ composeApp/src/commonMain/kotlin/dev/sikzyo/weather_app_kmp/ui/theme/Color.kt
@@ -0,0 +1,98 @@
+package dev.sikzyo.weather_app_kmp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Colores de Radix_UI
+
+object lightColors {
+    val gray_2 = Color(0xFFF9F9F9)
+    val gray_3 = Color(0xFFF0F0F0)
+    val gray_4 = Color(0xFFE8E8E8)
+    val gray_5 = Color(0xFFE0E0E0)
+    val gray_7 = Color(0xFFCECECE)
+    val gray_8 = Color(0xFFBBBBBB)
+    val gray_10 = Color(0xFF838383)
+    val gray_12 = Color(0xFF202020)
+
+    val mauve_2 = Color(0xFFFAF9FB)
+    val mauve_3 = Color(0xFFF2EFF3)
+    val mauve_7 = Color(0xFFD0CDD7)
+    val mauve_10 = Color(0xFF84828E)
+    val mauve_11 = Color(0xFF65636D)
+    val mauve_12 = Color(0xFF211F26)
+
+    val slate_1 = Color(0xFFFCFCFD)
+    val slate_2 = Color(0xFFF9F9FB)
+    val slate_3 = Color(0xFFF0F0F3)
+    val slate_5 = Color(0xFFE0E1E6)
+    val slate_12 = Color(0xFF1C2024)
+
+    val sand_2 = Color(0xFFF9F9F8)
+    val sand_3 = Color(0xFFF1F0EF)
+    val sand_12 = Color(0xFF21201C)
+
+    val red_5 = Color(0xFFFFCDCE)
+
+    val purple_12 = Color(0xFF402060)
+
+    val indigo_8 = Color(0xFF8DA4EF)
+
+    val blue_1 = Color(0xFFFBFDFF)
+    val blue_2 = Color(0xFFF4FAFF)
+    val blue_3 = Color(0xFFE6F4FE)
+    val blue_4 = Color(0xFFD5EFFF)
+    val blue_5 = Color(0xFFC2E5FF)
+    val blue_12 = Color(0xFF113264)
+
+    val amber_3 = Color(0xFFFFF7C2)
+    val amber_10 = Color(0xFFFFBA18)
+    val amber_12 = Color(0xFF4F3422)
+
+    val yellow_2 = Color(0xFFFEFCE9)
+    val yellow_3 = Color(0xFFFFFAB8)
+    val yellow_4 = Color(0xFFFFF394)
+    val yellow_12 = Color(0xFF473B1F)
+
+    val sky_3 = Color(0xFFE1F6FD)
+    val sky_12 = Color(0xFF1D3E56)
+}
+
+object darkColors {
+    val gray_2 = Color(0xFF191919)
+    val gray_3 = Color(0xFF222222)
+    val gray_4 = Color(0xFF2A2A2A)
+    val gray_5 = Color(0xFF313131)
+    val gray_8 = Color(0xFF606060)
+    val gray_10 = Color(0xFF7B7B7B)
+    val gray_12 = Color(0xFFEEEEEE)
+
+    val mauve_3 = Color(0xFF232225)
+    val mauve_12 = Color(0xFFEEEEF0)
+
+    val slate_2 = Color(0xFF18191B)
+    val slate_3 = Color(0xFF212225)
+    val slate_5 = Color(0xFF2E3135)
+    val slate_12 = Color(0xFFEDEEF0)
+
+    val sand_2 = Color(0xFF191918)
+    val sand_3 = Color(0xFF222221)
+    val sand_12 = Color(0xFFEEEEEC)
+
+    val red_5 = Color(0xFF611623)
+
+    val purple_12 = Color(0xFFECD9FA)
+
+    val blue_1 = Color(0xFF0D1520)
+    val blue_2 = Color(0xFF111927)
+    val blue_3 = Color(0xFF0D2847)
+    val blue_4 = Color(0xFF003362)
+    val blue_5 = Color(0xFF004074)
+    val blue_12 = Color(0xFFC2E6FF)
+
+    val yellow_2 = Color(0xFF1B180F)
+    val yellow_3 = Color(0xFF2D2305)
+    val yellow_12 = Color(0xFFF6EEB4)
+
+    val sky_3 = Color(0xFF112840)
+    val sky_12 = Color(0xFFC2F3FF)
+}
